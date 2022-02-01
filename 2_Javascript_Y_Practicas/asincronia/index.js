2335a8604320dea0c04adaab0138cb7fb09a077d
Luis Lechuga <luislechu55@gmail.com>
2022-01-31 21:38:36 -0600

    feat: primeros ejemplos de asincronia

---
+++ 2_Javascript_Y_Practicas/asincronia/index.js
@@ -0,0 +1,43 @@
+// Codigo ejecutado linea tras linea por el eventloop
+
+/*
+
+console.log('1 Inicio');
+console.log('2 Ejecutando');
+console.log('3 Terminando');
+
+*/
+
+
+// Ejecucion que toma tiempo siempre al event 
+
+/*
+
+console.log('1 Inicio');
+setTimeout(() => {
+    console.log('2 Ejecutando');
+}, 0000);
+console.log('3 Terminando');
+
+*/
+
+
+//Solucion 1 Callbacks (NO ES RECOMENDADA por casos de callbackhell)
+function run() {
+    console.log('1 Inicio');
+    myFuncion(ejecutando);
+    console.log('4 Fin');
+}
+
+function myFuncion(ejecutando) {
+    setTimeout(() => {
+        console.log('2 Ejecutando');
+        ejecutando();
+    }, 0000);
+}
+
+function ejecutando() {
+    console.log('3 Terminando');
+}
+
+run();
