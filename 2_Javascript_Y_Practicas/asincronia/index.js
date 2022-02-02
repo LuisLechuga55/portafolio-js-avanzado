c67b56cf583f47430b8a21e69500edd91058cc70
Luis Lechuga <luislechu55@gmail.com>
2022-02-01 19:07:47 -0600

    feat: bloque 4 de asincronia, usando promesas

--- 2_Javascript_Y_Practicas/asincronia/index.js
+++ 2_Javascript_Y_Practicas/asincronia/index.js
@@ -23,21 +23,50 @@ console.log('3 Terminando');
 
 
 //Solucion 1 Callbacks (NO ES RECOMENDADA por casos de callbackhell)
-function run() {
-    console.log('1 Inicio');
-    myFuncion(ejecutando);
-    console.log('4 Fin');
+// function run() {
+//     console.log('1 Inicio');
+//     myFuncion(ejecutando);
+//     // console.log('4 Fin');
+// }
+
+// function myFuncion(ejecutando) {
+//     setTimeout(() => {
+//         console.log('2 Ejecutando');
+//         ejecutando();
+//     }, 0000);
+// }
+
+// function ejecutando() {
+//     console.log('3 Terminando');
+//     console.log('4 Fin');
+// }
+
+// run();
+
+// Solucion 2 Promesas
+// Promise: Algo que va a ocurrir pero no sabemos cuando va a ocurrir.
+// Estados de las promesas: Pendiente (pending), resuelto (resolve) y rechazado (reject)
+
+function asincronia() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('2 Ejecutando');
+            resolve();
+            // reject(new Error('mi custom error'));
+        }, 1000);
+    });
 }
 
-function myFuncion(ejecutando) {
-    setTimeout(() => {
-        console.log('2 Ejecutando');
-        ejecutando();
-    }, 0000);
-}
-
-function ejecutando() {
+console.log('1 Inicio');
+asincronia().then(() => {
+    console.log('en el then');
     console.log('3 Terminando');
-}
+    // Siguientes acciones despues de que responde la promesa con resolve()
 
-run();
+}).catch(error =>  console.log('error: ', error))
+
+.finally(() => {
+    // Se ejecuta siempre, tanto si la promesa fue resolve o reject
+    console.log('4 FIN');
+    console.log('en el finally');
+})
