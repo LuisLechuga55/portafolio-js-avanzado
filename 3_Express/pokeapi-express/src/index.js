414cf1e10d0d40fd221c01057c5e3a4faf4badf2
Luis Lechuga <luislechu55@gmail.com>
2022-02-09 13:51:10 -0600

    feat: primeros endpoints de trainner

--- 3_Express/pokeapi-express/src/index.js
+++ 3_Express/pokeapi-express/src/index.js
@@ -1,30 +1,12 @@
 const express = require("express");
+const routerApi = require('./routes');
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-	res.send("hola mundo!");
-});
-
-app.get("/trainer", (req, patito) => {
-	const objeto = { id: 1, name: "jose montoya", age: 32 };
-	patito.json(objeto);
-});
+app.use(express.json());
 
 app.listen(port, () => {
 	console.log(`Server ok, puerto: ${port}`);
 });
 
-/**
- *
- * Este código resultaria en un error porque a y b no están definidas
- * // let a = 1; let b = 2;
- *
- * if (a == b ) {
- * 	return true
- * }
- *
- */
-
-
-console.log('Hola mundo');
+routerApi(app);
---
+++ 3_Express/pokeapi-express/src/routes/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const dummyRoutes = require('./dummy/dummy.router.js');
+const trainerRoutes = require('./trainers/trainers.router');
+const pokemonRoutes = require('./pokemones/pokemones.router');
+
+function routerApi(app) {
+    const router = express.Router();
+    app.use('/api/v1', router);
+    router.use('/dummyes', dummyRoutes);
+    router.use('/trainers', trainerRoutes);
+    router.use('/pokemones', pokemonRoutes);
+    router.use('/centros', pokemonRoutes);
+}
+
+module.exports = routerApi;
+
+// http://localhost:3000/api/v1/trainers/ GET
+// http://localhost:3000/api/v1/trainers/:idTrainer/region/:idRegion GET
+// http://localhost:3000/api/v1/trainers/?page=10&size=1 GET
+// http://localhost:3000/api/v1/trainers/ POST
+
+// DELETE => URL
+// PATCH => URL
+// PUT => URL
+
+// create, read, update, delete => CRUD
